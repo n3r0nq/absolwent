6cf1aa1822bd7a725972b95e0944105301063d84
n3r0nq <nerofsg@gmail.com>
2019-03-19 14:32:41 +0100

    komit ostatni z wtorku 5 marca

--- osoby/admin.py
+++ osoby/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from osoby import models
+
+
+admin.site.register(models.Klasa)
+admin.site.register(models.Absolwent)
--- osoby/models.py
+++ osoby/models.py
@@ -8,6 +8,20 @@ class Klasa(models.Model):
     rok_matury = models.IntegerField("rok matury", default=0)
     rok_naboru = models.IntegerField("rok naboru", default=0)
 
+    class Meta:
+        verbose_name_plural = 'klasy'
+        ordering = ['rok_matury']
+
+    def __str__(self):
+        return self.nazwa + "(" + str(self.rok_matury) + ")"
+
+
 class Absolwent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     klasa = models.ForeignKey(Klasa, on_delete=models.SET_NULL, blank=True, null=True, related_name="uczniowie")
+
+    class Meta:
+        verbose_name_plural = 'absolwenci'
+
+    def __str__(self):
+        return self.user.get_full_name()
